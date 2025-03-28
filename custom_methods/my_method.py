66c10266c3291bbcb35258dffae6be11a20e9ef8
Jay Gokani <jay.gokani@coretus.com>
2025-03-28 15:16:42 +0530

    added entire bff

--- custom_methods/my_method.py
+++ custom_methods/my_method.py
@@ -1,7 +1,9 @@
 from icecream import ic
 
 def my_method(request, request_data):
-    ic(request, request_data, request.headers)
+    ic(request, request_data)
+    for header_name, header_value in request.headers.items():
+        ic(header_name, header_value)
     ic("This is supposed to run before grpc method call")
     # Modify the password in request_data
     request_data["password"] = "Password@123"
